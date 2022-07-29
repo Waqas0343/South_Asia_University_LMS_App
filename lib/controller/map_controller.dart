c219117bd6444f12afa528639d9ca98d549e9700
Muhammad Waqas <waqasjafar@instacare.software>
2022-07-29 22:02:18 +0500

    Remove Deprecated Package from Map

--- lib/controller/map_controller.dart
+++ lib/controller/map_controller.dart
@@ -1,180 +1,76 @@
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class GoogleMapController extends GetxController{
-
+class GoogleMapController extends GetxController {
   forDeanOffice() async {
-    const url = 'https://goo.gl/maps/AuZxjPguSqdmBtdd8';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/AuZxjPguSqdmBtdd8');
   }
 
   forMedia() async {
-    const url = 'https://goo.gl/maps/AuZxjPguSqdmBtdd8';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/AuZxjPguSqdmBtdd8');
   }
 
   forBioloy() async {
-    const url = 'https://goo.gl/maps/AuZxjPguSqdmBtdd8';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-  forNetworkingLab() async {
-    const url = 'https://goo.gl/maps/AuZxjPguSqdmBtdd8';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/AuZxjPguSqdmBtdd8');
   }
 
+  forNetworkingLab() async {
+    launchUrlString('https://goo.gl/maps/AuZxjPguSqdmBtdd8');
+  }
 
   forAdminBlock() async {
-    const url = 'https://goo.gl/maps/t7v2TZLuWiVBQ8Ve9';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/t7v2TZLuWiVBQ8Ve9');
   }
-
 
   forCafeteria() async {
-    const url = 'https://goo.gl/maps/miQmWqVWc3oj9iSa7';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/miQmWqVWc3oj9iSa7');
   }
 
-
   forGround() async {
-    const url = 'https://goo.gl/maps/LkhzWEzozn3Ru6eC6';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/LkhzWEzozn3Ru6eC6');
   }
 
   forCricketGround() async {
-    const url = 'https://goo.gl/maps/Qwwv22WDDqbEJpWZ8';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/Qwwv22WDDqbEJpWZ8');
   }
-
-
 
   forSportsComplex() async {
-    const url = 'https://goo.gl/maps/fnE7M3LWPWbuxLgY6';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/fnE7M3LWPWbuxLgY6');
   }
-
 
   forFountainPark() async {
-    const url = 'https://goo.gl/maps/pHbes44ngx3DkZPm6';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/pHbes44ngx3DkZPm6');
   }
-
-
 
   forMasjid() async {
-    const url = 'https://goo.gl/maps/LzfRf5KAyxpbEBc5A';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/LzfRf5KAyxpbEBc5A');
   }
-
 
   forRebotics() async {
-    const url = 'https://goo.gl/maps/FyBrx9qKxLYSCYobA';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/FyBrx9qKxLYSCYobA');
   }
-
-
 
   forNewBlock() async {
-    const url = 'https://goo.gl/maps/mA98i5kEbPYNenez7';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/mA98i5kEbPYNenez7');
   }
 
-
   forNewBlock2() async {
-    const url = 'https://goo.gl/maps/a8JYLAims54DnKQ27';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/a8JYLAims54DnKQ27');
   }
 
   forOldBuilding() async {
-    const url = 'https://goo.gl/maps/FziqeY7tWK5Tr3if7';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/FziqeY7tWK5Tr3if7');
   }
 
   forOldBuildingInside() async {
-    const url = 'https://goo.gl/maps/yeEhyT2kMFA1SKvFA';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/yeEhyT2kMFA1SKvFA');
   }
 
   forOldBuildingOutside() async {
-    const url = 'https://goo.gl/maps/RGPa9mKtugD9ujUC9';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/RGPa9mKtugD9ujUC9');
   }
 
   forUsman() async {
-    const url = 'https://goo.gl/maps/yvFNGPQpbgrYSUBc6';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    launchUrlString('https://goo.gl/maps/yvFNGPQpbgrYSUBc6');
   }
-
 }
-
